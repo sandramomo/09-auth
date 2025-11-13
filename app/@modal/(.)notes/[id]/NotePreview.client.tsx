6f7f242c4790981a3f8b2000e9530d2eb4ffeac0
Oleksandra <olexandra.momotiuk@gmail.com>
2025-11-14 00:04:13 +0200

    Edit path

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { fetchNoteById } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api/clientApi";
 import Modal from "@/components/Modal/Modal";
 import css from './NotePreview.module.css';
 
